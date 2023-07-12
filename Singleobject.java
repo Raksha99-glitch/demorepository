1bdb2a80f03b2efad1ef1d6a672a3cd2a9185d9f
Raksha99-glitch <139256653+Raksha99-glitch@users.noreply.github.com>
2023-07-12 19:52:05 +0530

    solution available for singleton design pattern

---
+++ Singleobject.java
@@ -0,0 +1,20 @@
+package com.singleton;
+
+public class Singleobject 
+{
+   private static Singleobject instance = new Singleobject();
+   
+   private Singleobject()
+   {
+   }
+   public static Singleobject getinstance()
+   {
+	   return instance;
+   }
+   public void showmessage()
+   {
+	   System.out.println("Edubridge india pvt Lid.");   
+   }
+	
+
+}
---
+++ Singletonpattern.java
@@ -0,0 +1,12 @@
+package com.singleton;
+
+public class Singletonpattern {
+
+  public static void main(String[] args)
+		{
+		     Singleobject s1 = Singleobject.getinstance();
+			
+	         s1.showmessage();
+		}
+}
+	
